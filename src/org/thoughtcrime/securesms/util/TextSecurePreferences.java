2a613d34672ab16b0553fb6e5066f2fbd853ccf1
Unknown <johanw@xs4all.nl>
2019-07-04 06:16:57 +0200

    Added encrypted and plaintext backup / restore. Re-added import SMS database. Removed apk expire. Added the choice between passphrase protection and the Android screenlock. Added more conversation colors and themes. Added a choice for the backup location.

--- src/org/thoughtcrime/securesms/util/TextSecurePreferences.java
+++ src/org/thoughtcrime/securesms/util/TextSecurePreferences.java
@@ -183,6 +183,31 @@ public class TextSecurePreferences {
 
   private static final String MEDIA_KEYBOARD_MODE = "pref_media_keyboard_mode";
 
+  // JW: added: true = passphrase, false = Android lock or fingerprint
+  public static final String PROTECTION_METHOD_PREF = "pref_signal_protection_method";
+  // JW: added: true = backup to removable SD card (if available), false = backup to internal sd card
+  public static final String BACKUP_LOCATION_REMOVABLE_PREF = "pref_backup_location_external";
+  
+  // JW: added for PROTECTION_METHOD_PREF
+  public static boolean isProtectionMethodPassphrase(@NonNull Context context) {
+    return getBooleanPreference(context, PROTECTION_METHOD_PREF, false);
+  }
+
+  // JW added
+  public static void setProtectionMethod(@NonNull Context context, boolean value) {
+    setBooleanPreference(context, PROTECTION_METHOD_PREF, value);
+  }
+
+  // JW added
+  public static void setBackupLocationRemovable(@NonNull Context context, boolean value) {
+    setBooleanPreference(context, BACKUP_LOCATION_REMOVABLE_PREF, value);
+  }
+
+  // JW added. Default to false so default does the same as official Signal.
+  public static boolean isBackupLocationRemovable(@NonNull Context context) {
+    return getBooleanPreference(context, BACKUP_LOCATION_REMOVABLE_PREF, false);
+  }
+
   public static boolean isScreenLockEnabled(@NonNull Context context) {
     return getBooleanPreference(context, SCREEN_LOCK, false);
   }
