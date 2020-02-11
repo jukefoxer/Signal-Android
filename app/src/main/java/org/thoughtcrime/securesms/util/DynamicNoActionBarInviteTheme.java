ffc9add6f38fb2a70a8efe1e7b1da5e28c715e6d
Unknown <johanw@xs4all.nl>
2020-02-12 00:34:40 +0100

    Added encrypted and plaintext backup / restore. Re-added import SMS database. Removed apk expire. Added the choice between passphrase protection and the Android screenlock. Added more conversation colors and themes. Added a choice for the backup location. Added an option to treat view-once media as normal media. Added an option to disable PIN reminders.

--- app/src/main/java/org/thoughtcrime/securesms/util/DynamicNoActionBarInviteTheme.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/DynamicNoActionBarInviteTheme.java
@@ -10,6 +10,9 @@ public class DynamicNoActionBarInviteTheme extends DynamicTheme {
     String theme = TextSecurePreferences.getTheme(activity);
 
     if (theme.equals("dark")) return R.style.Signal_NoActionBar_Invite;
+    else if (theme.equals("oled")) return R.style.Signal_NoActionBar_Invite_Oled; // JW: added
+    else if (theme.equals("green")) return R.style.Signal_Light_NoActionBar_Invite_Green; // JW: added
+    else if (theme.equals("blue")) return R.style.Signal_Light_NoActionBar_Invite_Blue; // JW: added
 
     return R.style.Signal_Light_NoActionBar_Invite;
   }
