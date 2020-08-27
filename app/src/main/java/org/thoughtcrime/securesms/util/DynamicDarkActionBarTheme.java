4b389815acacbedf2cf6d2ba3c9c226e84920a2f
Unknown <johanw@xs4all.nl>
2020-08-27 21:34:03 +0200

    Added extra options
    Added encrypted and plaintext backup / restore. Re-added import SMS database. Removed apk expire. Added the choice between passphrase protection and the Android screenlock. Added more conversation colors and themes. Added a choice for the backup location. Added an option to treat view-once media as normal media. Added an option to ignore remote deletion.

--- app/src/main/java/org/thoughtcrime/securesms/util/DynamicDarkActionBarTheme.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/DynamicDarkActionBarTheme.java
@@ -13,4 +13,18 @@ public class DynamicDarkActionBarTheme extends DynamicTheme {
   protected @StyleRes int getDarkThemeStyle() {
     return R.style.TextSecure_DarkTheme_Conversation;
   }
+
+  // JW: added
+  protected @StyleRes int getDarkThemeOledStyle() {
+    return R.style.TextSecure_DarkTheme_Conversation;
+  }
+  // JW: added
+  protected @StyleRes int getLightThemeBlueStyle() {
+    return R.style.TextSecure_LightTheme_Conversation;
+  }
+  // JW: added
+  protected @StyleRes int getLightThemeGreenStyle() {
+    return R.style.TextSecure_LightTheme_Conversation;
+  }
+
 }
