f4d85400238085fe972ce6ab65f5709c709bcb2d
Unknown <johanw@xs4all.nl>
2021-01-07 02:30:28 +0100

    Added extra options
    - Encrypted and plaintext backup / restore.
    - Import SMS database. Removed apk expire.
    - Choose between passphrase protection and the Android screenlock.
    - More conversation colors.
    - Choice for the backup location (internal or removable storage).
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message.

--- app/src/main/java/org/thoughtcrime/securesms/color/MaterialColor.java
+++ app/src/main/java/org/thoughtcrime/securesms/color/MaterialColor.java
@@ -15,43 +15,61 @@ import java.util.Map;
 
 import static org.thoughtcrime.securesms.util.ThemeUtil.isDarkTheme;
 
+// JW: put original colors back
 public enum MaterialColor {
-  CRIMSON    (R.color.conversation_crimson,     R.color.conversation_crimson_tint,     R.color.conversation_crimson_shade,     "red"),
-  VERMILLION (R.color.conversation_vermillion,  R.color.conversation_vermillion_tint,  R.color.conversation_vermillion_shade,  "orange"),
-  BURLAP     (R.color.conversation_burlap,      R.color.conversation_burlap_tint,      R.color.conversation_burlap_shade,      "brown"),
-  FOREST     (R.color.conversation_forest,      R.color.conversation_forest_tint,      R.color.conversation_forest_shade,      "green"),
-  WINTERGREEN(R.color.conversation_wintergreen, R.color.conversation_wintergreen_tint, R.color.conversation_wintergreen_shade, "light_green"),
-  TEAL       (R.color.conversation_teal,        R.color.conversation_teal_tint,        R.color.conversation_teal_shade,        "teal"),
-  BLUE       (R.color.conversation_blue,        R.color.conversation_blue_tint,        R.color.conversation_blue_shade,        "blue"),
-  INDIGO     (R.color.conversation_indigo,      R.color.conversation_indigo_tint,      R.color.conversation_indigo_shade,      "indigo"),
-  VIOLET     (R.color.conversation_violet,      R.color.conversation_violet_tint,      R.color.conversation_violet_shade,      "purple"),
-  PLUM       (R.color.conversation_plumb,       R.color.conversation_plumb_tint,       R.color.conversation_plumb_shade,       "pink"),
-  TAUPE      (R.color.conversation_taupe,       R.color.conversation_taupe_tint,       R.color.conversation_taupe_shade,       "blue_grey"),
-  STEEL      (R.color.conversation_steel,       R.color.conversation_steel_tint,       R.color.conversation_steel_shade,       "grey"),
+  RED        (R.color.red_700,                  R.color.red_400,                       R.color.red_900,                        "red"),
+  PINK       (R.color.pink_700,                 R.color.pink_400,                      R.color.pink_900,                       "pink"),
+  PURPLE     (R.color.purple_700,               R.color.purple_400,                    R.color.purple_900,                     "purple"),
+  DEEP_PURPLE(R.color.deep_purple_700,          R.color.deep_purple_400,               R.color.deep_purple_900,                "deep_purple"),
+  INDIGO     (R.color.indigo_700,               R.color.indigo_400,                    R.color.indigo_900,                     "indigo"),
+  BLUE       (R.color.blue_700,                 R.color.blue_500,                      R.color.blue_900,                       "blue"),
+  LIGHT_BLUE (R.color.light_blue_700,           R.color.light_blue_500,                R.color.light_blue_900,                 "light_blue"),
+  CYAN       (R.color.cyan_700,                 R.color.cyan_500,                      R.color.cyan_900,                       "cyan"),
+  TEAL       (R.color.teal_700,                 R.color.teal_500,                      R.color.teal_900,                       "teal"),
+  GREEN      (R.color.green_700,                R.color.green_500,                     R.color.green_900,                      "green"),
+  LIGHT_GREEN(R.color.light_green_700,          R.color.light_green_600,               R.color.light_green_900,                "light_green"),
+  LIME       (R.color.lime_700,                 R.color.lime_500,                      R.color.lime_900,                       "lime"),
+  YELLOW     (R.color.yellow_700,               R.color.yellow_500,                    R.color.yellow_900,                     "yellow"),
+  AMBER      (R.color.amber_700,                R.color.amber_600,                     R.color.amber_900,                      "amber"),
+  ORANGE     (R.color.orange_700,               R.color.orange_500,                    R.color.orange_900,                     "orange"),
+  DEEP_ORANGE(R.color.deep_orange_700,          R.color.deep_orange_500,               R.color.deep_orange_900,                "deep_orange"),
+  BROWN      (R.color.brown_700,                R.color.brown_500,                     R.color.brown_900,                      "brown"),
+  GREY       (R.color.grey_700,                 R.color.grey_500,                      R.color.grey_900,                       "grey"),
+  STEEL      (R.color.grey_700,                 R.color.grey_500,                      R.color.grey_900,                       "grey"),
   ULTRAMARINE(R.color.conversation_ultramarine, R.color.conversation_ultramarine_tint, R.color.conversation_ultramarine_shade, "ultramarine"),
-  GROUP      (R.color.conversation_group,       R.color.conversation_group_tint,       R.color.conversation_group_shade,       "blue");
+  BLUE_GREY  (R.color.blue_grey_700,            R.color.blue_grey_500,                 R.color.blue_grey_900,                  "blue_grey"),
+  BLACK      (R.color.black,                    R.color.black,                         R.color.black,                          "black"),
+  TEALS      (R.color.conversation_teal,        R.color.conversation_teal_tint,        R.color.conversation_teal_shade,        "teals"),
+  BURLAP     (R.color.conversation_burlap,      R.color.conversation_burlap_tint,      R.color.conversation_burlap_shade,      "burlap"),
+  TAUPE      (R.color.conversation_taupe,       R.color.conversation_taupe_tint,       R.color.conversation_taupe_shade,       "taupe"),
+  GROUP      (R.color.blue_700,                 R.color.blue_500,                      R.color.blue_900,                       "blue");
 
+  // JW: no translation of colors
   private static final Map<String, MaterialColor> COLOR_MATCHES = new HashMap<String, MaterialColor>() {{
-    put("red",         CRIMSON);
-    put("deep_orange", CRIMSON);
-    put("orange",      VERMILLION);
-    put("amber",       VERMILLION);
-    put("brown",       BURLAP);
-    put("yellow",      BURLAP);
-    put("pink",        PLUM);
-    put("purple",      VIOLET);
-    put("deep_purple", VIOLET);
-    put("indigo",      INDIGO);
-    put("blue",        BLUE);
-    put("light_blue",  BLUE);
-    put("cyan",        TEAL);
-    put("teal",        TEAL);
-    put("green",       FOREST);
-    put("light_green", WINTERGREEN);
-    put("lime",        WINTERGREEN);
-    put("blue_grey",   TAUPE);
-    put("grey",        STEEL);
+    put("red", RED);
+    put("brown", BROWN);
+    put("pink", PINK);
+    put("purple", PURPLE);
+    put("deep_purple", DEEP_PURPLE);
+    put("indigo", INDIGO);
+    put("blue", BLUE);
+    put("light_blue", LIGHT_BLUE);
+    put("cyan", CYAN);
+    put("blue_grey", BLUE_GREY);
+    put("teal", TEAL);
+    put("green", GREEN);
+    put("light_green", LIGHT_GREEN);
+    put("lime", LIME);
+    put("orange", ORANGE);
+    put("amber", AMBER);
+    put("deep_orange", DEEP_ORANGE);
+    put("yellow", YELLOW);
+    put("grey", GREY);
+    put("black", BLACK);
     put("ultramarine", ULTRAMARINE);
+    put("teals", TEALS);
+    put("burlap", BURLAP);
+    put("taupe", TAUPE);
     put("group_color", GROUP);
   }};
 
